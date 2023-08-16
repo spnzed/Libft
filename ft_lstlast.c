ea980e62e1d891da6be9fe0480fd6003dc90a49f
Aaron Espinosa Garcés <95354392+spnzed@users.noreply.github.com>
2023-08-16 20:44:15 +0200

    Add files via upload

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -1,37 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strrncmp.c                                      :+:      :+:    :+:   */
+/*   ft_lstlast.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aaespino <aaespino@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/18 16:28:48 by aaespino          #+#    #+#             */
-/*   Updated: 2023/04/25 15:48:03 by aaespino         ###   ########.fr       */
+/*   Created: 2023/06/01 11:31:20 by aaronespino       #+#    #+#             */
+/*   Updated: 2023/06/25 01:15:44 by aaespino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_strrncmp(const char *str, const char *str2, size_t n)
+t_list  *ft_lstlast(t_list *lst)
 {
-	int	i;
-	int	l;
-
-	i = ft_strlen(str) - 1;
-	l = ft_strlen(str2) - 1;
-	while (n > 0)
-	{
-		if (str[i] == str2[l])
-		{
-			i--;
-			l--;
-			n--;
-		}
-		else
-			break ;
-	}
-	if (n == 0)
-		return (0);
-	else
-		return (1);
+    if (!lst)
+        return (NULL);
+    while (lst->next)
+        lst = lst->next;
+    return (lst);
 }
